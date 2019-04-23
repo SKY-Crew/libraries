9e2dca78fa0c37bb9247c84b3362ea03bf861d1b
SKY-Crew <43493783+SKY-Crew@users.noreply.github.com>
2019-04-24 00:47:40 +0900

    [Modify] Use template

--- Dist/Dist.h
+++ Dist/Dist.h
@@ -16,7 +16,7 @@ const uint8_t DIST_TYPE = 5;
 inline String str(const Dist& d) { return DIST_NAME[d]; }
 inline char toChar(const Dist& d) { return DIST_CHAR[d]; }
 
-inline Dist compare(uint16_t val, const uint16_t* THRE, uint16_t TYPE, Dist MIN) {
+template<typename T1, typename T2> inline Dist compare(T1 val, const T2* THRE, uint16_t TYPE, Dist MIN) {
 	uint16_t index = 0;
 	bool proportional = THRE[0] < THRE[TYPE - 2];
 	for (; index < TYPE - 1; index ++) {
