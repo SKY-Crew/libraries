33efd773ad9ef6b7d3df8cf6ff4167594d5e0aeb
SKY-Crew <43493783+SKY-Crew@users.noreply.github.com>
2019-06-22 16:35:06 +0900

    [Modify] set rotOpp false while cannot see goal

--- Original/Cam.cpp
+++ Original/Cam.cpp
@@ -53,7 +53,7 @@ cam_t Cam::get(bool isInAir) {
 							+str("\tposOwn:")+str(goal.sideOwn * goal.diffOwn)
 							+str("\tisInCorner:")+str(goal.isInCorner)); }
 		}
-		goal.rotOpp = bool(goal.rotOpp) ? filterAngle(crtRot, goal.rotOpp, 0.95) : crtRot;
+		goal.rotOpp = bool(goal.rotOpp) && bool(crtRot) ? filterAngle(crtRot, goal.rotOpp, 0.95) : crtRot;
 		goal.distOwn = crtDist != 0 ? filter(crtDist, goal.distOwn, 0.9) : goal.distOwn;
 	}else {
 		goal = {0, true, 0, 64, CENTER, NONE, CENTER};
